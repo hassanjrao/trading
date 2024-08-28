93684eb78c2f04b5cca777b3cb4a79daec131532
Hassan Javaid <hassanjrao@gmail.com>
2024-08-29 01:01:47 +0400

    compiles

--- database/seeders/FirmChallengeSeeder.php
+++ database/seeders/FirmChallengeSeeder.php
@@ -22,10 +22,6 @@ class FirmChallengeSeeder extends Seeder
                 'before_price'=>rand(100, 500),
                 'account_size_id'=>rand(1, 5),
                 'step_id'=>rand(1, 4),
-                'profit_target'=>rand(1000, 10000),
-                'max_daily_loss'=>rand(1000, 5000),
-                'max_daily_loss_note'=>NULL,
-                'max_total_drawdown'=>rand(1000, 5000),
                 'profit_split'=>'80% to 90%',
                 'activation_fee'=>rand(100, 500),
                 'rewards'=>'5 Points',
@@ -33,22 +29,5 @@ class FirmChallengeSeeder extends Seeder
         }
 
 
-        for($i=0; $i<40; $i++){
-            FirmChallenge::create([
-                'firm_id'=>2,
-                'actual_price'=>rand(100, 1000),
-                'actual_price_note'=>'MONTHLY',
-                'before_price'=>rand(100, 500),
-                'account_size_id'=>rand(1, 5),
-                'step_id'=>rand(1, 4),
-                'profit_target'=>rand(1000, 10000),
-                'max_daily_loss'=>rand(1000, 5000),
-                'max_daily_loss_note'=>'Daily Pause',
-                'max_total_drawdown'=>rand(1000, 5000),
-                'profit_split'=>'80% to 90%',
-                'activation_fee'=>rand(100, 500),
-                'rewards'=>'5 Points',
-            ]);
-        }
     }
 }
