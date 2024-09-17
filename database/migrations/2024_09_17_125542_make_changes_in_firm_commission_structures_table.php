ee2c6901a7cd0f1642012651ec9fa6c57710d6f2
Hassan Javaid <hassanjrao@gmail.com>
2024-09-17 17:37:53 +0400

    added commission str

--- database/migrations/2024_09_17_125542_make_changes_in_firm_commission_structures_table.php
+++ database/migrations/2024_09_17_125542_make_changes_in_firm_commission_structures_table.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Schema;
 
 class MakeChangesInFirmCommissionStructuresTable extends Migration
@@ -13,13 +14,15 @@ class MakeChangesInFirmCommissionStructuresTable extends Migration
      */
     public function up()
     {
+        // truncate table
+        // DB::table('firm_commission_structures')->truncate();
         Schema::table('firm_commission_structures', function (Blueprint $table) {
 
-            $table->dropColumn([
-                'image_path','name','points'
-            ]);
+            // $table->dropColumn([
+            //     'image_path','name','points'
+            // ]);
 
-            $table->foreignId('commission_structure_id')->constrained('commission_structures')->onDelete('cascade');
+            // $table->foreignId('commission_structure_id')->constrained('commission_structures')->onDelete('cascade');
 
         });
     }
