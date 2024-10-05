a6a5f8f79aed8895bdea5c5ad7050da76d79e2a1
Hassan Javaid <hassanjrao@gmail.com>
2024-10-05 23:16:41 +0400

    commissionn structure issue

--- resources/js/components/Firms/FirmAbout.vue
+++ resources/js/components/Firms/FirmAbout.vue
@@ -273,7 +273,12 @@ export default {
       this.firmAbout.payment_methods = this.firm.payment_methods.map((item) => item.id);
       this.firmAbout.payout_methods = this.firm.payout_methods.map((item) => item.id);
       this.firmAbout.platforms = this.firm.platforms.map((item) => item.id);
-      this.allFirmComssionStructures = this.firm.commission_structures;
+      this.allFirmComssionStructures = this.firm.commission_structures.map((item) => {
+        return {
+          commission_structure_id: item.commission_structure_id,
+          note: item.note,
+        };
+      });
     }
   },
   created() {
